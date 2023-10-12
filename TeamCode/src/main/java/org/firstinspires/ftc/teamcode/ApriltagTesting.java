59d5ae376168029412db9e64f5583504c2b47e40
prosenfeld <prosenfeld@yuhsbstudents.org>
2023-10-11 22:10:46 -0400

    Trying to undo the git goof part 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ApriltagTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ApriltagTesting.java
@@ -51,7 +51,7 @@ public class ApriltagTesting extends LinearOpMode {
 
     public static String COLOR_INACTIVE_TURN = "#7c4dff7a";
     // Prop Vision
-    private PropVision propVision = new PropVision();
+    private PropVision propVision = new PropVision(this.telemetry,false);
 
     FtcDashboard dashboard = FtcDashboard.getInstance();
     Telemetry dashboardTelemetry = dashboard.getTelemetry();
@@ -102,6 +102,8 @@ public class ApriltagTesting extends LinearOpMode {
         visionPortal.close();
 
     }   // end method runOpMode()
+
+
 
     /**
      * Initialize the AprilTag processor.
@@ -160,34 +162,6 @@ public class ApriltagTesting extends LinearOpMode {
 
     public void initPropVision(){
     }
-
-    /**
-     * Function to add telemetry about AprilTag detections.
-     */
-    private void telemetryAprilTag() {
-
-        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-        telemetry.addData("# AprilTags Detected", currentDetections.size());
-
-        // Step through the list of detections and display info for each one.
-        for (AprilTagDetection detection : currentDetections) {
-            if (detection.metadata != null) {
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-            } else {
-                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
-            }
-        }   // end for() loop
-
-        // Add "key" information to telemetry
-        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
-        telemetry.addLine("RBE = Range, Bearing & Elevation");
-
-    }   // end method telemetryAprilTag()
 
     public void processAprilTags(){
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
