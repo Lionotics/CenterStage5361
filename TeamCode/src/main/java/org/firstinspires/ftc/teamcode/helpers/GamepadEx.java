8c039696f8b387901d0352c995f851078de96e5c
prosenfeld <prosenfeld@yuhsbstudents.org>
2023-11-03 16:07:02 -0400

    Fixed gamepadEx issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/GamepadEx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/GamepadEx.java
@@ -4,25 +4,28 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class GamepadEx {
 
-    private Gamepad gamepad;
-
-    public Button b,x,y,up,down,left,right = new Button();
+//    public Button a,b,x,y,up,down,left,right = new Button();
     public Button a = new Button(false);
+    public Button b = new Button(false);
+    public Button x = new Button(false);
+    public Button y = new Button(false);
+    public Button up = new Button(false);
+    public Button down = new Button(false);
+    public Button left = new Button(false);
+    public Button right = new Button(false);
 
-    public GamepadEx(Gamepad gamepad){
-        this.gamepad = gamepad;
-    }
+    public GamepadEx(){}
 
-    public void update(Gamepad gamepad){
+    public void update(Gamepad updatedGamepad){
 
-        this.a.update(gamepad.a);
-        this.b.update(gamepad.b);
-        this.x.update(gamepad.x);
-        this.y.update(gamepad.y);
-        this.up.update(gamepad.dpad_up);
-        this.down.update(gamepad.dpad_down);
-        this.left.update(gamepad.dpad_left);
-        this.right.update(gamepad.dpad_right);
+        this.a.update(updatedGamepad.a);
+        this.b.update(updatedGamepad.b);
+        this.x.update(updatedGamepad.x);
+        this.y.update(updatedGamepad.y);
+        this.up.update(updatedGamepad.dpad_up);
+        this.down.update(updatedGamepad.dpad_down);
+        this.left.update(updatedGamepad.dpad_left);
+        this.right.update(updatedGamepad.dpad_right);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/DrivetrainTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/DrivetrainTesting.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.checkerframework.checker.units.qual.Speed;
 import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
 import org.firstinspires.ftc.teamcode.helpers.GamepadEx;
@@ -13,34 +14,36 @@ public class DrivetrainTesting extends LinearOpMode
 {
 //    Robot robot = new Robot();
     Drivetrain drive = new Drivetrain();
-    GamepadEx gamepadEx1 = new GamepadEx(gamepad1);
+    GamepadEx gamepadEx1 = new GamepadEx();
     @Override
     public void runOpMode() throws InterruptedException {
+
         drive.init(hardwareMap);
 
         waitForStart();
-        drive.setMaxSpeed(1);
+
+        drive.setMaxSpeed(0.5);
 
         while (opModeIsActive()){
 
+            // update our gamepad
+            gamepadEx1.update(gamepad1);
 
             // Actually drive the robot
             drive.drive(gamepad1.left_stick_y,gamepad1.left_stick_x,gamepad1.right_stick_x);
-//            gamepadEx1.update(gamepad1);
-//
-//
-//            // Speed down toggle
-//            if(gamepadEx1.a.isNewlyReleased() && drive.getMaxSpeed() > 0){
-//                drive.setMaxSpeed(drive.getMaxSpeed() - 0.1);
-//            }
-//            // Speed up toggle
-//            if(gamepadEx1.b.isNewlyPressed() && drive.getMaxSpeed() < 1){
-//                drive.setMaxSpeed(drive.getMaxSpeed() + 0.1);
-//            }
+
+//             Speed down toggle
+            if(gamepadEx1.a.isNewlyReleased() && drive.getMaxSpeed() > 0){
+                drive.setMaxSpeed(drive.getMaxSpeed() - 0.1);
+            }
+            // Speed up toggle
+            if(gamepadEx1.b.isNewlyPressed() && drive.getMaxSpeed() < 1){
+                drive.setMaxSpeed(drive.getMaxSpeed() + 0.1);
+            }
+
             // Show the speed to the user
             telemetry.addData("Max Speed:", drive.getMaxSpeed());
             telemetry.update();
-            // update our gamepad
         }
 
     }
